3cb3dbc6b509b4cba528e951c61191c9e7259b48
aa.tikhonov <minussinus@yandex.ru>
2023-12-05 22:07:32 +0300

    day 5

--- 05/a.js
+++ 05/a.js
@@ -1,5 +1,7 @@
 import { input_a as input_a } from "./input.js";
 
+console.time("solution");
+
 const data = input_a()
   .split("\n\n")
   .map((i) => i.trim().split(":"));
@@ -94,52 +96,10 @@ for (let map_i = 0; map_i < maps.length; map_i++) {
   output_main = [...output];
 }
 
-// const seeds_locations2 = seeds2.map((s) => {
-//   const res = maps.reduce(
-//     (val, map) => {
-//       const new_val = [];
-//       for (let j1 = 0; j1 < val.length; j1++) {
-//         let val_item = val[j1];
-//         const m_valid = map.filter(
-//           (mi) =>
-//             mi[1] <= val_item[0] + val_item[1] && val_item[0] <= mi[1] + mi[2]
-//         );
-//         if (!m_valid) {
-//           new_val.push(val_item);
-//         } else {
-//           val_item = m_valid.reduce((s, mi) => {
-//             const new_s = [];
-//             for (let i = 0; i < s.length; i++) {
-//               const b = Math.max(mi[1], s[i][0]);
-//               const e = Math.min(mi[1] + mi[2], s[i][0] + s[i][1]);
-//               new_val.push([b, e]);
-
-//               if (s[i][0] < mi[1]) {
-//                 new_s.push([s[i][0], mi[1] - s[i][0]]);
-//               }
-//               if (mi[1] + mi[2] < s[i][0] + s[i][1]) {
-//                 new_s.push([
-//                   mi[1] + mi[2] + 1,
-//                   s[i][0] + s[i][1] - (mi[1] + mi[2] + 1),
-//                 ]);
-//               }
-//             }
-
-//             return new_s;
-//           }, val_item);
-//         }
-//       }
-
-//       return new_val;
-//     },
-//     [s]
-//   );
-//   return res;
-// });
-
 seeds_locations.sort((b, a) => b - a);
 output_main.sort((b, a) => b[0] - a[0]);
-// seeds_locations2.sort((b, a) => b - a);
 
 console.log("A", seeds_locations[0]);
 console.log("B", output_main[0][0]);
+
+console.timeLog("solution");
